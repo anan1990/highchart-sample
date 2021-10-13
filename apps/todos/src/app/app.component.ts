05e35c078f611c84de30e68f2595d16c51b8b90b
An An <an@gmail.com>
2021-10-13 16:58:00 +0700

    Init project

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
-  selector: 'abc-root',
+  selector: 'abc-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todos';
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    colors: ['#000000', '#aaafd3'],
+    title: {
+      text: 'Fruit Consumption',
+    },
+    xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges'],
+    },
+    yAxis: {
+      title: {
+        text: 'Fruit eaten',
+      },
+    },
+    series: [
+      {
+        data: [1, 2, 3],
+        type: 'bar',
+        // color: '#abc431',
+        name: 'An An',
+      },
+      {
+        type: 'bar',
+        // color: '#1326df',
+        data: [1, 9, 4],
+      },
+      {
+        type: 'bar',
+        color: '#e1d336',
+        data: [5, 7, 3],
+      },
+    ],
+  };
 }
--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HighchartsChartModule } from 'highcharts-angular';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule, HighchartsChartModule],
   providers: [],
   bootstrap: [AppComponent],
 })
